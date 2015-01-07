792df92a8d471dc5257fd36598ae206ba8fca7f4
uwec <uwe650@hotmail.com>
2015-01-07 01:07:10 -0500

    adding bank modal

---
+++ client/js/controllers/bank_modal.js
@@ -0,0 +1,44 @@
+"use strict";
+app.controller('bank_modalCtrl',
+function ($scope, $modal, $log) {
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: '../views/bank_modal.html',
+      controller: 'Instance',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+app.controller('Instance', function ($scope, $modalInstance, items, $MongoDB) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $MongoDB.remove;
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+    $MongoDB.create();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
