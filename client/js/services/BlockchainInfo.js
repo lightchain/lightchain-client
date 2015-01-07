d3c1b2d2f7e17fd8ad60e867af6fc45c310c193e
uwec <uwe650@hotmail.com>
2015-01-07 01:09:03 -0500

    reimplementing as $resource

--- client/js/services/BlockchainInfo.js
+++ client/js/services/BlockchainInfo.js
@@ -1,103 +1,47 @@
 'use strict';
-app.service( "Blockchaininfo", function($resource) {
+app.factory("Blockchaininfo", ["$resource", "$window", "$http", function($resource, $window, $http) {
 
-//var Blockchain= function($http){
-  this.url='https://blockchain.info/';
-//};
-/*
- * Retrieves multiple addresses info
- * @param {String} Bitcoin addresses array
- * @param {Function} callback Function that will be called with multiAddr
- */
+  var baseUrl = "https://blockchain.info";
 
- this.multiAddr=function(addresses, callback){
-   var _url = this.url;
+  var resource = $resource(baseUrl + "/", {}, {
+            multiAddr: {
+                method: "GET",
+                url: baseUrl + '/multiaddr?active=:addresses',
+                params: {
+                    addresses: "@addresses"
+                }
+            },
+            getBlockCount: {
+                method: "GET",
+                isArray: false,
+                url: baseUrl + "/latestblock",
+            },
+            getTx: {
+                method: "GET",
+                isArray: false,
+                url: baseUrl + "/rawtx/:txHash",
+                params: {
+                    txHash: "@txHash"
+                }
+            },
+            getUnspent: {
+                method: "GET",
+                isArray: false,
+                url: baseUrl + "unspent?active=:addresses",
+                params:{ 
+                  addresses: "@addresses"
+                }
+            },
+            pushTx: {
+                method: "POST",
+                isArray: false,
+                url: baseUrl + "pushtx",
+                params: {
+                    post_data: "@post_data"
+                }
+            }
 
-
-
-  $http({method: 'GET', url: _url+'/multiaddr?active='+addresses.join( '|' ),headers : { 'Content-Type': 'application/x-www-form-urlencoded' }}).
-    success(function(data, status, headers, config) {
-      callback(data);
-    }).
-    error(function(data, status, headers, config) {
-      callback(data);
-    });
-}
-
-/*
- * Gets latest block count in blockchain
- * @param {Function} callback Function that will be called with getBlockCount
- */
-  this.getBlockCount = function(callback) {
-  var _url = this.url;
-  $http({method: 'GET', url: _url+'/latestblock',headers : { 'Content-Type': 'application/x-www-form-urlencoded' }}).
-    success(function(data, status, headers, config) {
-      callback(status,data);
-    }).
-    error(function(data, status, headers, config) {
-      callback(status,data);
-    });
-}
-
-/*
- * Gets raw transaction by transaction hash
- * @param {String} tx Transaction hash in hex
- * @param {Function} callback Function that will be called with getTx
- */
-  this.getTx = function(tx, callback) {
-  var _url = this.url;
-  $http({method: 'GET', url: _url+'/rawtx/'+tx,headers : { 'Content-Type': 'application/x-www-form-urlencoded' }}).
-    success(function(data, status, headers, config) {
-      callback(status,data);
-    }).
-    error(function(data, status, headers, config) {
-      callback(status,data);
-    });
-}
-
-/*
- * Gets all unspent outputs through blockchain.info
- * @param {String} Bitcoin addresses array
- * @param {Function} callback Function that will be called with getUnspent
- */
-
-  this.getUnspent= function(addresses, callback){
-    var _url = this.url;
-      $http({method: 'GET', url: _url + 'unspent?active='+addresses.join('|'),headers : { 'Content-Type': 'application/x-www-form-urlencoded' }}).
-    success(function(data, status, headers, config) {
-      callback(data.unspent_outputs);
-    }).
-    error(function(data, status, headers, config) {
-      callback(data);
-    });
-  }
-
- /*
-  * Broadcast transaction using blockchain.ino
-  * @param {String} tx_serialized hex code
-  * @param {String} tx_hash transaction hash
-  * @param {function} callback Function that will be called with getUnspent
-  */
-  this.pushTx =function(tx_serialized, tx_hash, callback) {
-    var _url = this.url;
-
-    var post_data = {
-      format: "plain",
-      tx: tx_serialized,
-      hash: tx_hash,
-    };
-      $http({method: 'POST', url: _url + 'pushtx',data:post_data,headers : { 'Content-Type': 'application/x-www-form-urlencoded' }}).
-    success(function(data, status, headers, config) {
-      callback(data);
-    }).
-    error(function(data, status, headers, config) {
-      callback(data);
-    });
-  }
+        });
 
 
- // return Blockchain;
-});//end factory
-/*module.exports = {
-Blockchain: Blockchain
-}*/
+}])
